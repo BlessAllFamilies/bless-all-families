8eed49942410415dfd9875c7e61639acd88170d8
blessallfamilies <blessallfamilies@gmail.com>
2025-02-03 07:31:13 +0000

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyA2Bop29ahaIdUTi2WFtsQmO28l4aD-Mv4',
+      authDomain: 'bless-all-families-ai-app.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'bless-all-families-ai-app',
+      storageBucket: 'bless-all-families-ai-app.firebasestorage.app',
+      messagingSenderId: '298347566231',
+      appId: '1:298347566231:web:7a76ca5ce2519e8da785d2',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyA3VRM7QOqUpJ-5a3xPPvS8odbNNOvqs3o',
+  debug_token: ''
+};
